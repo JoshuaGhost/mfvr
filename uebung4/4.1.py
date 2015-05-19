e6868f762718cf800bf5d4ff3a565b4b589f2aa2
Joshua Ghost <joshuaghost@gmail.com>
2015-05-19 23:08:00 +0200

    yifangwanyi

---
+++ uebung4/4.1.py
@@ -0,0 +1,97 @@
+import pygame
+from time import clock
+from math import sin, pi
+
+poolwidth = 100
+bar_width = 6
+level = 50
+waveheight = 50
+wavelen = 20
+
+offset_x = 100
+offset_y = 300
+
+delta_t = 0.01
+wavespeed = 4.0
+delta_x = 1.0
+
+waveseq = []
+for i in range(wavelen+1):
+    waveseq.append(waveheight*sin(i*pi/wavelen))
+
+class Game(object):
+    
+    def __init__(self):
+        pass    
+
+    def __intgrt__(self, h, v):
+        h[0] = h[1]
+        h[-1] = h[-2]
+        assert 1<=poolwidth-2
+        for i in range(1, poolwidth+1):
+            v[i] = v[i]+\
+                   (delta_t*wavespeed**2/delta_x**2)*\
+                   (h[i-1]-2*h[i]+h[i+1])
+        
+        for i in range(1, poolwidth+1):
+            h[i] = level+(h[i]+delta_t*v[i]-level)*(0.999)
+        return h, v
+        
+    def setwave(self, h, center):
+        for i in range(center-wavelen/2, center+wavelen/2+1):
+            h[i+1] = level+waveseq[i-wavelen/2-center]
+        return h
+
+    def main(self, screen):
+
+        x_scale, y_scals = screen.get_size()
+        h = [level] * (poolwidth+2)
+        v = [0]*(poolwidth+2)
+        wavecenter = wavelen/2
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                if event.type == pygame.KEYDOWN and\
+                    event.key == pygame.K_ESCAPE:
+                    return
+                if event.type == pygame.MOUSEMOTION:
+                    (mousex, mousey) = pygame.mouse.get_pos()
+                    wavecenter = (mousex-offset_x)/bar_width
+                    wavecenter = wavecenter if wavecenter-(wavelen/2)>0 else wavelen/2
+                    wavecenter = wavecenter if wavecenter+(wavelen/2)<poolwidth+1 else poolwidth-(wavelen/2)+1
+                if event.type == pygame.MOUSEBUTTONDOWN and\
+                    wavecenter <> wavelen/2 and\
+                    wavecenter <> poolwidth-(wavelen/2)+1 and\
+                    pygame.mouse.get_pressed()[0]:
+                    h = self.setwave(h, wavecenter)
+                
+            screen.fill((200, 200, 200))
+        
+            for i in range(1, len(h)-1):
+                try:
+                    pygame.draw.rect(screen, (0,0,255),\
+                                     (offset_x+i*bar_width,\
+                                      offset_y-int(h[i]),\
+                                      bar_width,\
+                                      int(h[i])),\
+                                     0)
+                except Exception, e:
+                    print i, ' ', h[i]
+                    print e
+                    return 1
+            if wavecenter <> wavelen/2 and\
+                wavecenter <> poolwidth-(wavelen/2)+1:
+                    for i in range(wavecenter-wavelen/2, wavecenter+wavelen/2+1):
+                        pygame.draw.rect(screen, (139, 71, 38),\
+                                         (offset_x+i*bar_width,\
+                                          offset_y-int(level+waveseq[i-wavecenter+wavelen/2]),\
+                                          bar_width,\
+                                          int(waveseq[i-wavecenter+wavelen/2])),\
+                                         0)
+            h, v = self.__intgrt__(h,v)
+            pygame.display.update()
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+Game().main(screen)
---
+++ uebung4/4.2mitquelle.py
@@ -0,0 +1,82 @@
+import pygame
+from time import clock
+from math import sin, cos, pi, log, e
+
+dom_len = 10
+temp_len = 80 
+grid_len = 5
+
+offset_x = 200
+offset_y = 100
+
+delta_t = 0.01
+tempspeed = 8.0
+delta_x = float(dom_len)/float(temp_len)
+
+dirc = ((-1,0),(1,0),(0,1),(0,-1))
+
+palette = [(p, 0, 255-p) for p in range(256)]
+
+env_temp = 11.0
+
+class Game(object):
+    def __intgrt__(self, temp, v):
+       
+        for i in range(1, temp_len+1):
+            temp[0][i] = temp[1][i]
+            temp[i][0] = temp[i][1]
+            temp[temp_len+1][i] = temp[temp_len][i]
+            temp[i][temp_len+1] = temp[i][temp_len]
+        temp[temp_len/2][temp_len+1] = 11.0
+        
+        for i in range(1, temp_len+1):
+            for j in range(1, temp_len+1):
+                t = 0
+                for k in range(len(dirc)):
+                    t = t+temp[i+dirc[k][0]][j+dirc[k][1]]
+                t = t - 4*temp[i][j]
+                v[i][j] = v[i][j]+(delta_t*tempspeed**2/delta_x**2)*t
+        
+        for i in range(1, temp_len+1):
+            for j in range(1, temp_len+1):
+                t = temp[i][j]+delta_t*v[i][j]
+                temp[i][j] = t if t<=env_temp else env_temp
+        return temp, v
+     
+    def main(self, screen):
+        x_scale, y_scals = screen.get_size()
+        temp = [([0.0]*(temp_len+2)) for i in range(temp_len+2)]
+        v = [([0.0]*(temp_len+2)) for i in range(temp_len+2)]
+        for i in range(1, temp_len+1):
+            x = float(i)*delta_x
+            for j in range(1, temp_len+1):
+                y = float(j)*delta_x
+                temp[i][j] = sin(x)+x*cos(y)
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                if event.type == pygame.KEYDOWN and\
+                    event.key == pygame.K_ESCAPE:
+                    return
+                
+            screen.fill((200, 200, 200))
+            for i in range(1, temp_len+1):
+                for j in range(1, temp_len+1):
+                    try:
+                        pygame.draw.rect(screen, palette[int((temp[i][j]+11.0)/22.0*255)],\
+                                     (offset_x+(i-1)*grid_len,\
+                                      offset_y+(j-1)*grid_len,\
+                                      grid_len,\
+                                      grid_len),\
+                                     0)
+                    except Exception, e:
+                        print '(',i,',',j,')',':',temp[i][j]
+                        print e
+                        return 1
+            temp, v = self.__intgrt__(temp,v)
+            pygame.display.update()
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+Game().main(screen)
